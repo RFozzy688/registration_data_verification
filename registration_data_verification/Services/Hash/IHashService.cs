bbd1b848b7660918411a32361f3fc6142b4a4bf7
RFozzy688 <pokerfozzy@gmail.com>
2024-04-12 17:13:51 +0300

    created hash services

---
+++ registration_data_verification/Services/Hash/IHashService.cs
@@ -0,0 +1,7 @@
+﻿namespace registration_data_verification.Services.Hash
+{
+    public interface IHashService
+    {
+        String Digest(String input);
+    }
+}
---
+++ registration_data_verification/Services/Kdf/IKdfService.cs
@@ -0,0 +1,8 @@
+﻿namespace registration_data_verification.Services.Kdf
+{
+    public interface IKdfService
+    {
+        void Config(int iterationCount, int dkLength);
+        String GetDerivedKey(String password, String salt);
+    }
+}
